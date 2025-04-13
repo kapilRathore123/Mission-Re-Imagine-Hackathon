caadf1cb0c7c243b258582d9d6dd3e564eeacfd8
Rahul Sahu <8305rsm@gmail.com>
2025-04-14 00:19:22 +0530

    agian changes by rahul

---
+++ script1.js
@@ -0,0 +1,8 @@
+// let items=document.querySelectorAll(".item");
+// items.forEach(item => {
+//     item.addEventListener("click",()=>{
+        
+//         let s=item.innerHTML;
+//         console.log(ar);
+//     })
+// });
